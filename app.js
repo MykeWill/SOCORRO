aad793ffe1ea6a6a7b5c343dd53cd022204d290a
Mykão <mykewillow@gmail.com>
2022-07-21 23:03:42 -0300

    socorro boy kk

---
+++ app.js
@@ -0,0 +1,45 @@
+//importar modulos
+
+let express = require('express')
+let {engine} = require('express-handlebars')
+let bodyParser = require('body-parser')
+let fetch = require('node-fetch')// utilizar o fetch na rota 
+
+let app = express()
+//body-parser
+app.use (bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.json())
+
+//especificar arquivos estaticos
+app.use(express.static(__dirname + '/publico'))
+
+//template
+app.engine('handlebars', engine());
+app.set('view engine', 'handlebars');
+app.set("views", "./views");
+
+//rotas base
+app.get('/', function(req,res){
+    fetch('http://localhost:3000/clientes', {method: 'GET'})
+    .then(resposta =>resposta.json())
+    .then(resposta => res.render('inicio', {dados:resposta}))
+})
+
+//rota de cadastro aqui o method vai ser post pq fou definido la no action de inicio.handlebars
+app.post('/cadastrar', function(req, res){
+    let nome = req.body.nome
+    let idade = req.body.idade
+    let dados = {'nome': nome, 'idade': idade};
+
+    fetch('http://localhost:3000/clientes', {
+        method:'POST',
+        body: JSON.stringify(dados),
+        Headers:{'contenty-Type': 'application/json'}
+    
+    })
+    .then(res.redirect('/'))
+   
+})
+
+//servidor
+app.listen(8080)
